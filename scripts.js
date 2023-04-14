6e0e97c4daff97e3112abbcadb4a97b57ed3e894
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-04-13 23:17:41 -0600

    Se agrega funcionalidad de array como base de datos a consultar en el juego

--- scripts.js
+++ scripts.js
@@ -8,15 +8,54 @@ function saltarlinea(){
     document.write("<br>");
 }
 
-let secreto = 5;
+let secreto = Math.round(Math.random() * 10);
 let input = document.querySelector("input");
+input.focus();
 
 function adivinar(){
 if(parseInt(input.value) == secreto){
     alert("Felicidades, adivinaste el numero secreto");
 
    }else{
-    alert("Lo siento, no adivinaste el numero secreto");
+    alert("Lo siento, no adivinaste el numero secreto "+ secreto + " intenta de nuevo");
 
    }
+   input.value = "";
+   input.focus();
+
+  
+   
 }
+
+let arraySecreto =[9,7,6,5];
+function consultarBase(){
+    let input = document.querySelector("input");
+    let numero = parseInt(input.value);
+    if(arraySecreto.indexOf(numero) != -1){
+         alert("El numero " + numero + " si esta en la base de datos");
+    }else{
+         alert("El numero " + numero + " no esta en la base de datos");
+    }
+    input.value = "";
+    input.focus();
+}
+/*
+<meta charset="UTF-8">
+
+<h1>Recetas de Armando</h1>
+
+<script>
+
+    var ingredientes = [];
+    var cantidad = parseInt(prompt("¿Cuántos ingredientes vas a añadir?"));
+    var contador = 1;
+
+    while( contador <= cantidad) {
+        var ingrediente = prompt("Informe el ingrediente " + contador);
+        ingredientes.push(ingrediente);
+        contador++;
+    }
+
+    console.log(ingredientes);
+
+</script> */
